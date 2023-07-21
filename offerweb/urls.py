e72748e896f6570c3efbe9d1be96981ab641d8e2
Krzysztof Budzichowski <budzichowskik@TES.LOCAL>
2023-07-21 09:13:05 +0200

    troche dodaje i edytuje indeksy

--- offerweb/urls.py
+++ offerweb/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from offerweb.views import wszystkie_oferty,nowa_oferta, glowna,edytuj_top_oferty,edytuj_oferte, kontrahent, nowy_kontrahent, edytuj_kontrahent, usun_kontrahenta, usun_oferte ,usun_indeks
+from offerweb.views import wszystkie_oferty,nowa_oferta, glowna,edytuj_top_oferty,edytuj_oferte, kontrahent, nowy_kontrahent, edytuj_kontrahent, usun_kontrahenta, usun_oferte ,usun_indeks, edytuj_indeks
 
 urlpatterns = [
     path('oferty/', wszystkie_oferty, name="oferty"),
@@ -13,6 +13,7 @@ urlpatterns = [
     path('edytuj_kontrahent/<int:id>/', edytuj_kontrahent, name ="edytuj_kontrahenta"),
     path('usun_kontrahent/<int:id>/', usun_kontrahenta, name="usun_kontrahenta"),
     path('usun_indeks/<int:id>/', usun_indeks, name="usun_indeks"),
+    path('edytuj_indeks//<int:id>/', edytuj_indeks, name= "edytuj_indeks"),
 
     
     path('', glowna),
